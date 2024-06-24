186399a8608d9e751c41fe6236d64fd3984b59fc
Shrabonnn <138008141+Shrabonnn@users.noreply.github.com>
2024-06-24 22:20:46 +0600

    demo
    demo commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# test01
+A sample command-line application with an entrypoint in `bin/`, library code
+in `lib/`, and example unit test in `test/`.
---
+++ test/test01_test.dart
@@ -0,0 +1,8 @@
+import 'package:test01/test01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
